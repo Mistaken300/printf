ccead4a953b9735f8fc94d77c8d90434e7ccad86
Mistaken300 <you@example.com>
2023-07-24 15:09:50 -0700

    handle_write_char.c

---
+++ handle_write_char.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * handle_write_char - Prints a single character.
+ * @c: The character to be printed.
+ * @buffer: Buffer array to handle printing.
+ * @flags: Active flags that affect printing behavior.
+ * @width: Width specification for formatting.
+ * @precision: Precision specifier for formatting (not used in this function).
+ * @size: Size specifier for argument casting (not used in this function).
+ *
+ * Return: The number of characters printed.
+ */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size)
+{ /* char is stored at left and paddind at buffer's right */
+	int i = 0;
+	char padd = ' ';
+
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (flags & F_ZERO)
+		padd = '0';
+
+	buffer[i++] = c;
+	buffer[i] = '\0';
+
+	if (width > 1)
+	{
+		buffer[BUFF_SIZE - 1] = '\0';
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFF_SIZE - i - 2] = padd;
+
+		if (flags & F_MINUS)
+			return (write(1, &buffer[0], 1) +
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
+		else
+			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+					write(1, &buffer[0], 1));
+	}
+
+	return (write(1, &buffer[0], 1));
+}
